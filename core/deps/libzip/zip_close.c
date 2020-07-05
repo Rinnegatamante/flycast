2d81d81be85cf996432516276da8b7a0ed063ac2
Rinnegatamante <rinnegatamante@gmail.com>
2020-07-05 13:33:04 +0200

    Several compilation fixes.

--- core/deps/libzip/zip_close.c
+++ core/deps/libzip/zip_close.c
@@ -307,10 +307,11 @@ zip_close(struct zip *za)
 	}
 	return -1;
     }
+#ifndef VITA
     mask = umask(0);
     umask(mask);
     chmod(za->zn, 0666&~mask);
-
+#endif
     _zip_free(za);
     free(temp);
     
