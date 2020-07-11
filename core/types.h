e34d8d59579bc2e4c8d576f1861dc9f08cb8dece
Rinnegatamante <rinnegatamante@gmail.com>
2020-07-11 14:29:34 +0200

    Nulling verify on Vita build to gain some cpu cycles.

--- core/types.h
+++ core/types.h
@@ -429,7 +429,11 @@ void os_DebugBreak(void);
 #ifndef NO_VERIFY
 #define verify(x) if((x)==false){ msgboxf("Verify Failed  : " #x "\n in %s -> %s : %d \n",MBX_ICONERROR,(__FUNCTION__),(__FILE__),__LINE__); dbgbreak;}
 #else
+#ifdef VITA
+#define verify(x)
+#else
 #define verify(x) if((x)==false){ msgboxf("Verify Failed  : " #x "\n in %s -> %s : %d \n",MBX_ICONERROR,(__FUNCTION__),(__FILE__),__LINE__); }
+#endif
 #endif
 
 #define die(reason) { msgboxf("Fatal error : %s\n in %s -> %s : %d \n",MBX_ICONERROR,(reason),(__FUNCTION__),(__FILE__),__LINE__); dbgbreak;}
