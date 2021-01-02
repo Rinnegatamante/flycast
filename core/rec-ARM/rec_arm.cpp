a3cc708e4ae8e98da05ecf1e162bcf6c1c02b59d
Rinnegatamante <rinnegatamante@gmail.com>
2021-01-02 01:41:30 +0100

    Increase code size to check for SMC to 4 bytes.

--- core/rec-ARM/rec_arm.cpp
+++ core/rec-ARM/rec_arm.cpp
@@ -2229,7 +2229,7 @@ void ngen_Compile_arm(RuntimeBlockInfo* block,bool force_checks, bool reset, boo
 	if (force_checks)
 	{
 #ifdef VITA
-		s32 sz = 2;
+		s32 sz = 4;
 #else
 		s32 sz = block->sh4_code_size;
 #endif
